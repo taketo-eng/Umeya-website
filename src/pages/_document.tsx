eab77aa7cf87ca2723ba88cf4da3fb7608917060
Taketo Yahiro <yahiro.t.eng@gmail.com>
2023-04-08 13:40:20 +0900

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
