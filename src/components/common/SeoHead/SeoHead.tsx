7c5d1a9f8e5fde801e91c49a577e58a043084ace
Taketo Yahiro <yahiro.t.eng@gmail.com>
2023-04-08 14:16:55 +0900

    set structured data

--- src/components/common/SeoHead/SeoHead.tsx
+++ src/components/common/SeoHead/SeoHead.tsx
@@ -1,20 +1,39 @@
 import Head from "next/head"
+import Script from "next/script"
 
 export const SeoHead = () => {
   return (
-    <Head>
-      <title>梅屋 | 八女市黒木町の伝統建築</title>
-      <meta name="description" content="" />
+    <>
+      <Head>
+        <title>梅屋 | 八女市黒木町の伝統建築</title>
+        <meta name="description" content="" />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@" />
-      <meta property="og:title" content="梅屋" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://umeya.life/" />
-      <meta property="og:description" content="description" />
-      <meta property="og:image" content="https://umeya.life/test.jpg" />
-
-      <meta name="robots" content="noindex,nofollow"></meta>
-    </Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@" />
+        <meta property="og:title" content="梅屋" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://umeya.life/" />
+        <meta property="og:description" content="description" />
+        <meta property="og:image" content="https://umeya.life/test.jpg" />
+        <meta name="robots" content="noindex,nofollow"></meta>
+      </Head>
+      <Script type="application/ld+json" id="structerd_data">{`
+      {{
+        "@context": "http://schema.org",
+        "@type": "LocalBusiness",
+        "name": "梅屋",
+        "image": "https://umeya.life/logo.png",
+        "url": "https://umeya.life",
+        "address': {
+            "@type": "PostalAddress",
+            "streetAddress": "黒木町黒木37",
+            "addressLocality": "八女市",
+            "addressRegion": "福岡県",
+            "postalCode": "834-1292",
+            "addressCountry": "JP"
+        }
+      }}
+      `}</Script>
+    </>
   )
 }
