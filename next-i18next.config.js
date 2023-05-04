63a5f7c650a57ff4c64da0099e4b26c699634e7b
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2023-05-04 23:16:18 +0900

    fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    i18n: {
-        locales: ["ja", "en"],
-        defaultLocale: "ja",
-    },
-};
+  i18n: {
+    locales: ["ja", "en"],
+    defaultLocale: "ja",
+    localePath: path.resolve("./public/locales"),
+  },
+}
