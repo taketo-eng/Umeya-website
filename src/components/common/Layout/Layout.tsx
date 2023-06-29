0cd50f77599e911a4705087d18d496088b0fa3c5
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2023-06-29 18:54:41 +0900

    SeoHeadコンポーネントをseoディレクトリに移動し、いらないファイルは削除

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -5,57 +5,42 @@ import { LocomotiveScrollProvider } from "react-locomotive-scroll"
 import Script from "next/script"
 import Link from "next/link"
 import Image from "next/image"
-import { SeoHead } from "../SeoHead"
+import { SeoHead } from "../../seo/SeoHead"
 
 type Props = {
-  children: ReactNode
+    children: ReactNode
 }
 
 export const Layout: FC<Props> = ({ children }) => {
-  const ref = useRef(null)
+    const ref = useRef(null)
 
-  return (
-    <>
-      <SeoHead />
-      <Script id="preload" strategy="lazyOnload">
-        {`
+    return (
+        <>
+            <SeoHead />
+            <Script id="preload" strategy="lazyOnload">
+                {`
           const wrapper = document.querySelector(".top_wrapper")
           wrapper.classList.remove("preload")
         `}
-      </Script>
-      <LocomotiveScrollProvider
-        options={{
-          smooth: false,
-          offset: ["25%", 0],
-        }}
-        watch={[]}
-        containerRef={ref}
-      >
-        <div className="bg_wrapper"></div>
-        <div
-          className="preload top_wrapper"
-          data-scroll-container
-          id="top"
-          ref={ref}
-        >
-          <Header />
-          <main className="font-noto">{children}</main>
-          <Footer />
-          <Link
-            scroll={false}
-            className="ripple fixed right-4 bottom-4 w-8 md:w-10"
-            href="#top"
-          >
-            <Image
-              className="w-full"
-              src="/top_btn.svg"
-              width={32}
-              height={32}
-              alt="top"
-            />
-          </Link>
-        </div>
-      </LocomotiveScrollProvider>
-    </>
-  )
+            </Script>
+            <LocomotiveScrollProvider
+                options={{
+                    smooth: false,
+                    offset: ["25%", 0],
+                }}
+                watch={[]}
+                containerRef={ref}
+            >
+                <div className="bg_wrapper"></div>
+                <div className="preload top_wrapper" data-scroll-container id="top" ref={ref}>
+                    <Header />
+                    <main className="font-noto">{children}</main>
+                    <Footer />
+                    <Link scroll={false} className="ripple fixed right-4 bottom-4 w-8 md:w-10" href="#top">
+                        <Image className="w-full" src="/top_btn.svg" width={32} height={32} alt="top" />
+                    </Link>
+                </div>
+            </LocomotiveScrollProvider>
+        </>
+    )
 }
--- src/components/seo/top.tsx
+++
@@ -1,8 +0,0 @@
-import { NextSeo } from "next-seo"
-import { useTranslation } from "react-i18next"
-
-export const TopPageSeo = () => {
-  const { t } = useTranslation("seo")
-  const title = t("top.title")
-  return <NextSeo title={title} />
-}
