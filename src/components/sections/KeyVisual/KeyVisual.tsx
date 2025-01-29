8ace9284c9229e174490b6eba7bbce157f2e3bf8
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2025-01-29 16:37:45 +0900

    Update KeyVisual.tsx

--- src/components/sections/KeyVisual/KeyVisual.tsx
+++ src/components/sections/KeyVisual/KeyVisual.tsx
@@ -1,5 +1,6 @@
 import { getSetting } from '@/helpers/getSetting'
 import { getCurrentLocale } from '@/locales/server'
+import Image from 'next/image'
 import React from 'react'
 
 
@@ -10,10 +11,14 @@ export const KeyVisual = async () => {
 
     return (
         <div className="keyvisual">
-            <picture>
-            <source media="(max-width:767px)" width="500" height="800" srcSet={settings.kv_sp.url} />
-            <img width="1920" height="1080" className="w-full" src={settings.kv_pc.url} alt="" />
-            </picture>
+            {/* <picture>
+                <source media="(max-width:767px)" width="500" height="800" srcSet={settings.kv_sp.url} />
+                <img loading="lazy" width="1920" height="1080" className="w-full" src={settings.kv_pc.url} alt="" />
+            </picture> */}
+            <div>
+                <Image className="w-full md:hidden" alt="古民家梅屋キービジュアル SP" width={settings.kv_sp.width} height={settings.kv_sp.height} src={settings.kv_sp.url} blurDataURL={`${settings.kv_sp.url}?w=10&q=10`} />
+                <Image className="w-full hidden md:block" alt="古民家梅屋キービジュアル PC" width={settings.kv_pc.width} height={settings.kv_pc.height} src={settings.kv_pc.url} blurDataURL={`${settings.kv_pc.url}?w=10&q=10`} />
+            </div>
             <div className={`catch ${lang}`}>
             <p
                 className="font-serif"
