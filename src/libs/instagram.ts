a5cf763d28e8cb63b4adfe278be8d5925d3cdec8
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2025-03-02 18:54:14 +0900

    set limit

--- src/libs/instagram.ts
+++ src/libs/instagram.ts
@@ -1,7 +1,7 @@
 export const getInstagramPosts = async () => {
     try {
         const URL = "https://graph.facebook.com/v22.0/"
-        const target = `${URL}${process.env.INSTAGRAM_ID}/media?fields=caption,permalink,thumbnail_url,media_type,media_url&access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}`
+        const target = `${URL}${process.env.INSTAGRAM_ID}/media?fields=caption,permalink,thumbnail_url,media_type,media_url&limit=12&access_token=${process.env.INSTAGRAM_ACCESS_TOKEN}`
         const res = await fetch(target)
         if (!res.ok) {
             throw new Error('Failed to fetch data; Message')
