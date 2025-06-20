e9d159cb119ba1aaf759424d902c976fb9802dd4
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2025-06-21 00:22:03 +0900

    kv loading  lazy -> eager

--- src/components/sections/KeyVisual/KeyVisual.tsx
+++ src/components/sections/KeyVisual/KeyVisual.tsx
@@ -11,8 +11,8 @@ export const KeyVisual = async () => {
     return (
         <div className="keyvisual">
             <div>
-                <Image className="w-full md:hidden" alt="古民家梅屋キービジュアル SP" width={500} height={756} src="/kv/kv_sp.webp" />
-                <Image className="w-full hidden md:block" alt="古民家梅屋キービジュアル PC" width={1915} height={957} src="/kv/kv.webp" />
+                <Image className="w-full md:hidden" alt="古民家梅屋キービジュアル SP" width={500} height={756} src="/kv/kv_sp.webp" loading="eager" />
+                <Image className="w-full hidden md:block" alt="古民家梅屋キービジュアル PC" width={1915} height={957} src="/kv/kv.webp" loading="eager" />
             </div>
             <div className={`catch ${locale}`}>
                 <p
